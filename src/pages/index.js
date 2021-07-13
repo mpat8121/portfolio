8e0de3d894c90d908d9af02032ea0191749567df
patto <michaelpatterson@autotechfm.com>
2021-07-13 22:33:16 +1000

    Remove excess files, modify other content

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,8 @@ class IndexPage extends React.Component {
         </p>
         <p>I'm a fullstack dev who has been working for the same small IT shop in Sydney, Australia for more than 10 years...<br/>
         I also run <a href="https://www.interapptive.com.au" target="_blank">Interapptive</a> on the side for funsies.</p>
-        <p>My tech stack is mostly Node.Js, C#, Angular, Ionic with a little bit of Java and Swift thrown in for Capacitor plugin development. I also use Azure for all my DevOps needs.</p>
+        <p>My tech stack is mostly Node.Js, C#, Angular, Ionic with a little bit of Java and Swift thrown in for Capacitor plugin development. SQL Server, MySQL and MongoDB are my data stores of choice. 
+          I also use Azure for all my DevOps needs.</p>
         <p>My work with InterApptive focusses on mobile apps, with our current project being <a href="https://defectwise.com.au" target="_blank">DefectWise</a></p>
         <p>This blog is short snippets of utility functions I use often, small tips I've found whilst building stuff, along with some case studies of my projects.</p>
         <Link to="/blog/">
