1b419f380b63ecd188615b0577e672a1d527e1ff
patto <michaelpatterson@autotechfm.com>
2021-07-13 17:34:31 +1000

    Add analytics tracking and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,8 +107,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-150549522-1`,
       },
     },
     {
