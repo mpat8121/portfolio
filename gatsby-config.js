9d3cec6e390d450b357ba5adb2b018eca378f588
patto <michaelpatterson@autotechfm.com>
2021-08-03 17:43:39 +1000

    feat: Add dark mode plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-dark-mode',
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
