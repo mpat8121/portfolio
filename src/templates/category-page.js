0f227cb746dcccb629906bf647a6364d273ae948
patto <michaelpatterson@autotechfm.com>
2021-11-28 11:37:01 +1100

    Build fixes

--- src/templates/category-page.js
+++ src/templates/category-page.js
@@ -1,10 +1,44 @@
+import { graphql } from "gatsby"
 import React from "react"
 
 class CategoryPageTemplate extends React.Component {
     render() {
         console.log(this.props, this.context, this.state)
-        return<div></div>
+        return <div></div>
     }
 }
 
 export default CategoryPageTemplate
+
+export const pageQuery = graphql`
+query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+        edges {
+          node {
+            excerpt
+            fields {
+              slug
+            }
+            frontmatter {
+              date(formatString: "MMMM DD, YYYY")
+              title
+              description
+              categories
+              image {
+                childImageSharp {
+                  sizes(maxWidth: 600) {
+                    ...GatsbyImageSharpSizes
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+}
+`
