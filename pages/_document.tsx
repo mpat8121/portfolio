19bbd56f7cee28e611d6fcdaa4c93110bbe4b45e
Mick Patterson <michael@interapptive.com.au>
2022-08-11 15:46:51 +1000

    change netlify cms admin setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,27 +27,11 @@ const CustomDocument = () => {
   return (
     <Html>
       <Head>
-        <script
-          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-          async
-        ></script>
       </Head>
       <body>
         <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
         <Main />
         <NextScript />
-        <script dangerouslySetInnerHTML={{
-            __html: `
-              if (window.netlifyIdentity) {
-                window.netlifyIdentity.on("init", user => {
-                  if (!user) {
-                    window.netlifyIdentity.on("login", () => {
-                      document.location.href = "/admin/";
-                    });
-                  }
-                });
-              }
-          `}}/>
       </body>
     </Html>
   )
